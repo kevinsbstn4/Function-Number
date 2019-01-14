665768b3c563b001b6ff0bbccb446c2e163cb7be
kevinsbstn4 <kevinsbstn4@gmail.com>
2019-01-14 11:23:38 +0700

    function number

---
+++ index.js
@@ -0,0 +1,15 @@
+//performing task
+
+function greet(name,  lastName ) {
+    console.log('Hello' + name + '' + lastName);
+}
+
+greet("Santren", "Koding");
+
+// calculating a value
+
+function square(Number) {
+   return Number * Number;
+}
+
+console.log(square(5))
